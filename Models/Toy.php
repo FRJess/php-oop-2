054871bbb55439d298ac1d4fa474ff0a320e10c4
FR_Jess <fr_jess@icloud.com>
2022-12-12 18:05:23 +0100

    [add] all cards

--- Models/Toy.php
+++ Models/Toy.php
@@ -4,12 +4,14 @@ class Toy extends Product
 {
   public $feature;
   public $size;
+  public $category;
 
-  public function __construct($_id, $_name, $_brand, $_price, $_feature, $_size)
+  public function __construct($_id, $_name, $_brand, $_price, $_feature, $_size, Category $_category)
   {
     parent::__construct($_id, $_name, $_brand, $_price);
 
     $this->feature = $_feature;
     $this->size = $_size;
+    $this->category = $_category;
   }
 }
--- Views/partials/product.php
+++ Views/partials/product.php
@@ -6,37 +6,91 @@
 
   <main>
 
-  <div class="container">
+    <div class="container jt-container">
 
-    <div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+      <div class="row row-cols-1 row-cols-md-2 g-4 py-5">
 
-    <?php foreach($food_products as $product): ?>
-      
-      <div class="col">
-        <div class="card jt-card text-bg-light border-success h-100">
-          
-          <div class="card-header">
-            <h5 class=""><?php echo $product->name . ' (' . $product->brand . ')'?></h5>
-            <h6 class=""><?php echo $product->id ?></h6>
-          </div>
+      <?php foreach($food_products as $product): ?>
+        
+        <div class="col">
+          <div class="card jt-card text-bg-light border-dark h-100">
             
-            <div class="card-body">
-            <span class="badge text-bg-success position-absolute top-0 end-0 m-1"><?php echo $product->category->name ?></span>
-            <p class="">Ingredienti: <?php echo $product->ingredients ?></p>
-            <p class="">Sapore: <?php echo $product->taste ?></p>
-            <p class="">Peso: <?php echo $product->weight . 'kg' ?></p>
-              <p class="">Prezzo: <?php echo $product->price . '€'  ?></p>
-
+            <div class="card-header">
+              <h5 class=""><?php echo $product->name . ' (' . $product->brand . ')'?></h5>
+              <h6 class=""><?php echo $product->id ?></h6>
             </div>
+              
+              <div class="card-body">
+              <span class="badge text-bg-success position-absolute top-0 end-0 m-1"><?php echo $product->category->name ?></span>
+              <p class="">Ingredienti: <?php echo $product->ingredients ?></p>
+              <p class="">Sapore: <?php echo $product->taste ?></p>
+              <p class="">Peso: <?php echo $product->weight . 'kg' ?></p>
+              
+            </div>
+            <div class="card-footer text-center">
+              <div class="text-danger fw-bold"><?php echo $product->price . '€'  ?></div>
+            </div>
+          </div>
         </div>
+
+      <?php endforeach; ?>
       </div>
 
-    <?php endforeach; ?>
- 
+      <div class="row row-cols-1 row-cols-md-2 g-4 py-5">
+
+      <?php foreach($toy_products as $product): ?>
+        
+        <div class="col">
+          <div class="card jt-card text-bg-light border-dark h-100">
+            
+            <div class="card-header">
+              <h5 class=""><?php echo $product->name . ' (' . $product->brand . ')'?></h5>
+              <h6 class=""><?php echo $product->id ?></h6>
+            </div>
+              
+              <div class="card-body">
+              <span class="badge text-bg-primary position-absolute top-0 end-0 m-1"><?php echo $product->category->name ?></span>
+              <p class="">Ideale per: <?php echo $product->feature ?></p>
+              <p class="">Taglia: <?php echo $product->size ?></p>
+              
+            </div>
+            <div class="card-footer text-center">
+              <div class="text-danger fw-bold"><?php echo $product->price . '€'  ?></div>
+            </div>
+          </div>
+        </div>
+
+      <?php endforeach; ?>
+      </div>
+
+      <div class="row row-cols-1 row-cols-md-2 g-4 py-5">
+
+      <?php foreach($accessory_products as $product): ?>
+        
+        <div class="col">
+          <div class="card jt-card text-bg-light border-dark h-100">
+            
+            <div class="card-header">
+              <h5 class=""><?php echo $product->name . ' (' . $product->brand . ')'?></h5>
+              <h6 class=""><?php echo $product->id ?></h6>
+            </div>
+              
+              <div class="card-body">
+              <span class="badge text-bg-info position-absolute top-0 end-0 m-1"><?php echo $product->category->name ?></span>
+              <p class="">Materiale: <?php echo $product->material ?></p>
+              <p class="">Taglia: <?php echo $product->size ?></p>
+              
+            </div>
+            <div class="card-footer text-center">
+              <div class="text-danger fw-bold"><?php echo $product->price . '€'  ?></div>
+            </div>
+          </div>
+        </div>
+
+      <?php endforeach; ?>
+      </div>
 
     </div>
-
-  </div>
 
   </main>
 
