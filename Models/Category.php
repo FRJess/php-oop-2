b07b729c162c8d63366650a802fb4a4739f19ad1
FR_Jess <fr_jess@icloud.com>
2022-12-12 15:52:54 +0100

    [add] icons

--- Models/Category.php
+++ Models/Category.php
@@ -3,11 +3,19 @@
 class Category
 {
   public $name;
-  // public $icon;
+  public $icon;
 
   public function __construct($_name)
   {
     $this->name = $_name;
-    // $this->icon = $_icon;
+    $this->setIcon();
+  }
+  public function setIcon()
+  {
+      if ($this->type === 'Cane') {
+          $this->icon = 'https://cdn.icon-icons.com/icons2/2070/PNG/512/dog_icon_125586.png';
+      } elseif ($this->type === 'Gatto') {
+          $this->icon = 'https://cdn.icon-icons.com/icons2/2070/PNG/512/cat_icon_125794.png';
+      }
   }
 }
