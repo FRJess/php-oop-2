7d3e4b57f7e1d93fb4270f13049f32b99305bc97
FR_Jess <fr_jess@icloud.com>
2022-12-12 14:58:23 +0100

    [add] food model

---
+++ Models/Food.php
@@ -0,0 +1,17 @@
+<?php
+
+class Food extends Product
+{
+  public $weight;
+  public $ingredients;
+  public $taste;
+
+  public function __construct($_id, Category $_category, $_type, $_brand, $_name, $_cost, $_image, $_isAvailable, $_weight, $_ingredients, $_taste)
+  {
+    parent::__construct($_id, $_category, $_type, $_brand, $_name, $_cost, $_image, $_isAvailable);
+
+    $this->weight = $_weight;
+    $this->ingredients = $_ingredients;
+    $this->_taste = $_taste;
+  }
+}
--- index.php
+++ index.php
@@ -3,3 +3,13 @@
 - I prodotti sono categorizzati, le **categorie** sono Cani o Gatti.
 - I prodotti saranno oltre al **cibo**, anche **giochi**, **cucce**, etc.
 Stampiamo delle card contenenti i dettagli dei prodotti, come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando (prodotto, cibo, gioco, cuccia). -->
+
+
+<?php
+
+require __DIR__ . '/Models/Product.php';
+require __DIR__ . '/Models/Category.php';
+require __DIR__ . '/Models/Food.php';
+
+$yumyum = new Product('1234', new Category('cani'), 'cibo', 'Trainer', 'Natural', 20, 'null', true);
+var_dump($yumyum);
