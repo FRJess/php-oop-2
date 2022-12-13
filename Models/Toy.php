915d60e126ff749e3a67dad60fe172ac2e0f64f8
FR_Jess <fr_jess@icloud.com>
2022-12-13 09:58:35 +0100

    [update] toy variables

--- Models/Toy.php
+++ Models/Toy.php
@@ -1,17 +1,18 @@
 <?php
 
+require_once __DIR__ . '/Product.php';
+
 class Toy extends Product
 {
   public $feature;
   public $size;
-  public $category;
 
-  public function __construct($_id, $_name, $_brand, $_price, $_feature, $_size, Category $_category)
+
+  public function __construct($_id, String $_name, $_brand, Float $_price, String $_image, Category $_category, Bool $_isAvailable, Int $_quantity, String $_feature, String $_size)
   {
-    parent::__construct($_id, $_name, $_brand, $_price);
+    parent::__construct($_id, $_name, $_brand, $_price, $_image, $_category, $_isAvailable, $_quantity);
 
     $this->feature = $_feature;
     $this->size = $_size;
-    $this->category = $_category;
   }
 }
