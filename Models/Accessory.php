fc5a1531f02d6bb1bbbe4b1ec49d2f09f7dbd912
FR_Jess <fr_jess@icloud.com>
2022-12-13 09:59:25 +0100

    [update] accessory variables

--- Models/Accessory.php
+++ Models/Accessory.php
@@ -1,17 +1,17 @@
 <?php
 
+require_once __DIR__ . '/Product.php';
+
 class Accessory extends Product
 {
   public $material;
   public $size;
-  public $category;
 
-  public function __construct($_id, $_name, $_brand, $_price, $_material, $_size, Category $_category)
+  public function __construct($_id, String $_name, $_brand, Float $_price, String $_image, Category $_category, Bool $_isAvailable, Int $_quantity, String $_material, String $_size)
   {
-    parent::__construct($_id, $_name, $_brand, $_price);
+    parent::__construct($_id, $_name, $_brand, $_price, $_image, $_category, $_isAvailable, $_quantity);
 
     $this->material = $_material;
     $this->size = $_size;
-    $this->category = $_category;
   }
 }
